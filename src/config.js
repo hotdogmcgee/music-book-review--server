488504225eb2f9647496af2cc2ad7571dcc16ad8
Kevin Robinson <kevinrobinsondeveloper@gmail.com>
2020-01-23 10:11:51 -0500

    update db_url to database_url across files

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || "postgres://bookmaster:default@localhost/musicbooks",
+    DATABASE_URL: process.env.DATABASE_URL || "postgres://bookmaster:default@localhost/musicbooks",
     JWT_SECRET: process.env.JWT_SECRET || 'absolutely-change-this'
   }
